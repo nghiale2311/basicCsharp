c87cb6ece0dd8b23fb5bc74388ae69ab3c8584a3
nghiale2311 <138131065+nghiale2311@users.noreply.github.com>
2023-07-01 21:05:03 +0700

    BT loi chao

---
+++ CGO_Buoi01/CGO_Buoi01/Program.cs
@@ -0,0 +1,30 @@
+﻿//Ghi chu: using thư viện sài. Nếu chữ xám thư viện chưa sài 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+
+namespace CGO_Buoi01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Mời bạn nhập tên: ");
+            string sHoTen = Console.ReadLine();
+            Console.WriteLine("Tên của bạn là" + sHoTen );
+            Console.ReadKey(); 
+        }
+    }
+}
