1f45756a3891cfa397cd66a30daed5a180726128
nghiale2311 <138131065+nghiale2311@users.noreply.github.com>
2023-07-05 20:39:50 +0700

    CGO_Buoi02

---
+++ CGO_Buoi02/CGO_Buoi02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dem_sai = 3;
+
+            if (dem_sai > 0) Console.WriteLine(">0");
+            else Console.WriteLine("<0");
+            //toan tu 3 ngoi
+            string s = (dem_sai > 0) ? ">0" : "<0";
+            Console.WriteLine(s);
+        nhap_dob:
+            Console.Write("Nhap ngay sinh cua ban: ");
+            string input = Console.ReadLine();
+
+            DateTime dob;       //ngày sn bản thân
+            //if (DateTime.TryParse(input, out dob) == false) {
+            if (!DateTime.TryParse(input, out dob))
+            {
+                Console.WriteLine("Du lieu bi sau!");
+                dem_sai--;
+                if (dem_sai > 0) goto nhap_dob;
+                else
+                {
+                    Console.WriteLine("Het luot nhap lai!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+
+            DateTime dob_year = new DateTime(DateTime.Now.Year, dob.Month, dob.Day); //ngày sn trong năm
+            if (dob_year > DateTime.Now)
+                Console.WriteLine("Ngay sinh nhat sap toi, con " + (dob_year - DateTime.Now).TotalDays);
+            else if (dob_year == DateTime.Now.Date)
+                Console.WriteLine("Chuc mung sinh nhat ban!");
+            else
+            {
+                dob_year = dob_year.AddYears(1); //do đã qua ngày sn, nên tính năm tiếp theo
+                Console.WriteLine("Ngay sinh nhat sap toi, con " + (dob_year - DateTime.Now).TotalDays);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CGO_Buoi02/CGO_Buoi02Docsothanhchu/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi02Docsothanhchu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+     string input = Console.Readline();
+    int sonhap = int.Parse(input);
+    //29 -> 0 2 9
+    int donvi = sonhap % 10;			
+    int hangchuc = (sonhap/10) % 10;	
+    int hangtram = sonhap % 100;		
+
+    if(input.Length == 3){
+       //Xuat gia hangtram;
+       switch(hangtram){
+           case 0: Console.Write("zero"); break;
+           case 1: Console.Write("one hundred"); break;
+           //...
+       }
+    }
+    else if(input.Length == 2){
+       //Xuat gia hangchuc;
+       switch(hangchuc){
+           case 0: Console.Write("zero .."); break;
+           case 1: Console.Write("one hundred"); break;
+           //...
+       }
+    }
+   else  if(input.Length == 1){
+       //Xuat gia donvi;
+       switch(donvi){
+           case 0: Console.Write("zero .."); break;
+           case 1: Console.Write("one hundred"); break;
+           //...
+       }
+    }
+        
+        }
+
+
+
+
+
+
+
+
+
