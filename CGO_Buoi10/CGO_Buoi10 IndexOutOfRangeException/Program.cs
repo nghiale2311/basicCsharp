d65d4ae3d917073a0713f32d3c6d29f31b5003a2
nghiale2311 <138131065+nghiale2311@users.noreply.github.com>
2023-07-22 21:06:26 +0700

    Nop Bai

--- CGO_Buoi10/CGO_Buoi10 IndexOutOfRangeException/Program.cs
+++ CGO_Buoi10/CGO_Buoi10 IndexOutOfRangeException/Program.cs
@@ -10,6 +10,16 @@ namespace CGO_Buoi10_IndexOutOfRangeException
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
         }
     }
 }
