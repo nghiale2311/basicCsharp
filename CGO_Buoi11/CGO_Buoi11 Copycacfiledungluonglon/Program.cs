ef5ead2ce2fd1d457f85d07eee77de679a3c48b9
nghiale2311 <138131065+nghiale2311@users.noreply.github.com>
2023-07-27 15:40:56 +0700

    Nop Bai

---
+++ CGO_Buoi11/CGO_Buoi11 Copycacfiledungluonglon/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi11_Copycacfiledungluonglon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter source file: ");
+            string sourcePath = Console.ReadLine();
+            Console.WriteLine("Enter destination file: ");
+            string destinationPath = Console.ReadLine();
+
+            FileInfo source = new FileInfo(sourcePath);
+            FileInfo des = new FileInfo(destinationPath);
+            try
+            {
+                CopyFileUsingFileInfo(source, des);
+                Console.WriteLine("Copy Completed");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Cannot Copy");
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+        private static void CopyFileUsingFileInfo(FileInfo source, FileInfo des)
+        {
+            source.CopyTo(des.FullName, true);
+        }
+        private static void CopyFileUsingStream(FileInfo source, FileInfo des)
+        {
+            StreamReader reader = null;
+            StreamWriter writer = null;
+            try
+            {
+                reader = new StreamReader(source.FullName);
+                writer = new StreamWriter(des.FullName);
+                Char[] buffer = new Char[1024];
+                int length;
+                while ((length = reader.Read(buffer)) > 0) 
+                {
+                    writer.Write(buffer, 0, length);
+                }
+            }
+            finally
+            {
+                reader.Close();
+                reader.Dispose();
+                writer.Close();
+                writer.Dispose();
+            }
+        }
+    }
+}
